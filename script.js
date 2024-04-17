8afcaf55bdbdc101f9876db8b7a870e4cb9bd6a7
Yashraj_K <yashrajkamble7727@gamil.com>
2024-04-17 15:12:25 +0530

    js done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hasdsakdfj")
+
+let number = document.getElementById("number");
+let counter = 0;
+setInterval(() => {
+    if (counter == 65) {
+        clearInterval();
+    }
+    else {
+        counter += 1;
+        number.innerHTML = counter + "%";
+    }
+}, 30)
